ea794ea6e4bea966de16ad6b9bc087bfb44ec7c4
Martin Sumner <martin.sumner@adaptip.co.uk>
2025-01-20 21:29:05 +0000

    Update following review

--- src/riak_kv_http_cache.erl
+++ src/riak_kv_http_cache.erl
@@ -21,6 +21,7 @@
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
+-spec get_stats(Milliseconds :: pos_integer()) -> list({atom(), term()}).
 get_stats(Timeout) ->
     gen_server:call(?MODULE, get_stats, Timeout).
 
@@ -47,9 +48,10 @@ check_cache(#st{ts = undefined} = S) ->
     Stats = riak_kv_status:get_stats(web),
     S#st{ts = os:timestamp(), stats = Stats};
 check_cache(#st{ts = Then} = S) ->
-    CacheTime = application:get_env(riak_kv, http_stats_cache_seconds, 1),
+    CacheTime =
+        application:get_env(riak_kv, http_stats_cache_milliseconds, 1000),
     Now = os:timestamp(),
-    case timer:now_diff(Now, Then) < (CacheTime * 1000000) of
+    case timer:now_diff(Now, Then) < (CacheTime * 1000) of
         true ->
             S;
         false ->
